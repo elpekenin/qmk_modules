ec2e11f7cfa5145b89466df25421b8883da90067
Drashna Jaelre <drashna@live.com>
2025-04-16 11:39:46 -0700

    Add 8-bit mod_config check

--- indicators/indicators.c
+++ indicators/indicators.c
@@ -25,6 +25,43 @@ static bool should_draw_indicator(const indicator_t *indicator, const indicator_
     return true;
 }
 
+static uint8_t mod_config_8bit(uint8_t mod) {
+    if (keymap_config.swap_lalt_lgui) {
+        /** If both modifiers pressed or neither pressed, do nothing
+         * Otherwise swap the values
+         */
+        if (((mod & MOD_BIT(KC_LALT)) && !(mod & MOD_BIT(KC_LGUI))) ||
+            (!(mod & MOD_BIT(KC_LALT)) && (mod & MOD_BIT(KC_LGUI)))) {
+            mod ^= (MOD_BIT(KC_LALT) | MOD_BIT(KC_LGUI));
+        }
+    }
+    if (keymap_config.swap_ralt_rgui) {
+        if (((mod & MOD_BIT(KC_RALT)) && !(mod & MOD_BIT(KC_RGUI))) ||
+            (!(mod & MOD_BIT(KC_RALT)) && (mod & MOD_BIT(KC_RGUI)))) {
+            mod ^= (MOD_BIT(KC_RALT) | MOD_BIT(KC_RGUI));
+        }
+    }
+    if (keymap_config.swap_lctl_lgui) {
+        if (((mod & MOD_BIT(KC_LCTL)) && !(mod & MOD_BIT(KC_LGUI))) ||
+            (!(mod & MOD_BIT(KC_LCTL)) && (mod & MOD_BIT(KC_LGUI)))) {
+            mod ^= (MOD_BIT(KC_LCTL) | MOD_BIT(KC_LGUI));
+        }
+    }
+    if (keymap_config.swap_rctl_rgui) {
+        if (((mod & MOD_BIT(KC_RCTL)) && !(mod & MOD_BIT(KC_RGUI))) ||
+            (!(mod & MOD_BIT(KC_RCTL)) && (mod & MOD_BIT(KC_RGUI)))) {
+            mod ^= (MOD_BIT(KC_RCTL) | MOD_BIT(KC_RGUI));
+        }
+    }
+    if (keymap_config.no_gui) {
+        mod &= ~MOD_BIT(KC_LGUI);
+        mod &= ~MOD_BIT(KC_RGUI);
+    }
+
+    return mod;
+}
+
+
 bool draw_indicators(uint8_t led_min, uint8_t led_max) {
     uint8_t mods  = get_mods();
     uint8_t layer = get_highest_layer(layer_state);
@@ -34,7 +71,7 @@ bool draw_indicators(uint8_t led_min, uint8_t led_max) {
 #endif
 
     indicator_fn_args_t args = {
-        .mods  = mods,
+        .mods  = mod_config_8bit(mods),
         .layer = layer,
     };
 
