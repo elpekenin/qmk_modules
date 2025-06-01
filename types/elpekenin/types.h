577f9752846ed1b4241b89441badff1e92a5fd95
elpekenin <elpekenin@elpekenin.dev>
2025-06-01 21:25:59 +0200

    types: add `const` and "hide" fields

--- types/elpekenin/types.h
+++ types/elpekenin/types.h
@@ -12,18 +12,24 @@
 
 #pragma once
 
-#include <quantum/compiler_support.h>
-#include <quantum/util.h>
 #include <stdbool.h>
-#include <sys/cdefs.h>
 
 #include "printf/printf.h"
+
+_Noreturn static inline void raise_error(const char *msg) {
+    printf("[ERROR] %s\n", msg);
+    while (true) {
+    }
+}
 
 //
 // Result
 //
 
 #define _Result(T, E) result___##T##___##E
+
+#define result_unwrap(T, E) result___##T##___##E##_unwrap
+#define result_unwrap_err(T, E) result___##T##___##E##_unwrap_err
 
 /**
  * Define a new result type.
@@ -32,15 +38,32 @@
  *     T: The type of values.
  *     E: The type of errors.
  */
-#define ResultImpl(T, E)    \
-    typedef struct PACKED { \
-        bool is_ok;         \
-        union {             \
-            T __value;      \
-            E __error;      \
-        };                  \
-    } Result(T, E);         \
-    STATIC_ASSERT(offsetof(Result(T, E), is_ok) == 0, "Invalid data layout")
+#define ResultImpl(T, E)                                                         \
+    typedef struct Result(T, E) Result(T, E);                                    \
+                                                                                 \
+    struct Result(T, E) {                                                        \
+        const bool is_ok;                                                        \
+        union {                                                                  \
+            const T __value;                                                     \
+            const E __error;                                                     \
+        };                                                                       \
+        const T (*unwrap)(Result(T, E)) __attribute__((warn_unused_result));     \
+        const E (*unwrap_err)(Result(T, E)) __attribute__((warn_unused_result)); \
+    };                                                                           \
+                                                                                 \
+    static inline T result_unwrap(T, E)(Result(T, E) result) {                   \
+        if (result.is_ok) {                                                      \
+            return result.__value;                                               \
+        }                                                                        \
+        raise_error("Called `unwrap` on `Err`");                                 \
+    }                                                                            \
+                                                                                 \
+    static inline E result_unwrap_err(T, E)(Result(T, E) result) {               \
+        if (!result.is_ok) {                                                     \
+            return result.__error;                                               \
+        }                                                                        \
+        raise_error("Called `unwrap_err` on `Ok`");                              \
+    }
 
 /**
  * Result type.
@@ -52,17 +75,17 @@
 /**
  * Create a result with the given value.
  */
-#define Ok(T, E, v)                    \
-    (Result(T, E)) {                   \
-        .is_ok = true, .__value = (v), \
+#define Ok(T, E, v)                                                                                          \
+    (Result(T, E)) {                                                                                         \
+        .is_ok = true, .__value = (v), .unwrap = result_unwrap(T, E), .unwrap_err = result_unwrap_err(T, E), \
     }
 
 /**
  * Create a result instance with the given error.
  */
-#define Err(T, E, e)                    \
-    (Result(T, E)) {                    \
-        .is_ok = false, .__error = (e), \
+#define Err(T, E, e)                                                                                          \
+    (Result(T, E)) {                                                                                          \
+        .is_ok = false, .__error = (e), .unwrap = result_unwrap(T, E), .unwrap_err = result_unwrap_err(T, E), \
     }
 
 /**
@@ -75,18 +98,29 @@
 
 #define _Option(T) option___##T
 
+#define option_unwrap(T) option___##T##_unwrap
+
 /**
  * Define a new option type.
  *
  * Args:
  *     T: The type of values.
  */
-#define OptionImpl(T)       \
-    typedef struct PACKED { \
-        bool is_some;       \
-        T    __value;       \
-    } Option(T);            \
-    STATIC_ASSERT(offsetof(Option(T), is_some) == 0, "Invalid data layout")
+#define OptionImpl(T)                                                     \
+    typedef struct Option(T) Option(T);                                   \
+                                                                          \
+    struct Option(T) {                                                    \
+        const bool is_some;                                               \
+        const T    __value;                                               \
+        const T (*unwrap)(Option(T)) __attribute__((warn_unused_result)); \
+    };                                                                    \
+                                                                          \
+    static inline T option_unwrap(T)(Option(T) option) {                  \
+        if (option.is_some) {                                             \
+            return option.__value;                                        \
+        }                                                                 \
+        raise_error("Called `unwrap` on `None`");                         \
+    }
 
 /**
  * Option type.
@@ -98,57 +132,18 @@
 /**
  * Create an option with the given value.
  */
-#define Some(T, v)                       \
-    (Option(T)) {                        \
-        .is_some = true, .__value = (v), \
+#define Some(T, v)                                                   \
+    (Option(T)) {                                                    \
+        .is_some = true, .__value = (v), .unwrap = option_unwrap(T), \
     }
 
 /**
  * Create an empty option.
  */
-#define None(T)           \
-    (Option(T)) {         \
-        .is_some = false, \
+#define None(T)                                       \
+    (Option(T)) {                                     \
+        .is_some = false, .unwrap = option_unwrap(T), \
     }
-
-/**
- * ----
- */
-
-// -- barrier --
-
-_Noreturn static inline void raise_error(const char *msg) {
-    printf("[ERROR] %s\n", msg);
-    while (true) {
-    }
-}
-
-/**
- * Get the inner value from an `Ok`/`Some` value. Panic if `Err`/`None`.
- */
-#define unwrap(v)                                              \
-    ({                                                         \
-        /* is_ok / is_some */                                  \
-        /* relies on them being first element of struct */     \
-        bool *ptr = (bool *)&(v);                              \
-        if (!(*ptr)) {                                         \
-            raise_error("called `unwrap` on `Err` or `None`"); \
-        }                                                      \
-                                                               \
-        (v).__value;                                           \
-    })
-
-/**
- * Get the inner value from an `Err`. Panic if `Ok`.
- */
-#define unwrap_err(v)                                   \
-    ({                                                  \
-        if ((v).is_ok) {                                \
-            raise_error("called `unwrap_err` on `Ok`"); \
-        }                                               \
-                                                        \
-        (v).__error;                                    \
-    })
 
 /**
  * ----
@@ -171,51 +166,51 @@ _Noreturn static inline void raise_error(const char *msg) {
  * Args:
  *     T: The type of values.
  */
-#define RingBufferImpl(T)                                                     \
-    typedef struct RingBuffer(T) RingBuffer(T);                               \
-                                                                              \
-    struct RingBuffer(T) {                                                    \
-        T           *data;                                                    \
-        const size_t size;                                                    \
-        size_t       head;                                                    \
-        size_t       tail;                                                    \
-        bool (*push)(RingBuffer(T) *, T) __attribute__((warn_unused_result)); \
-        Option(T) (*pop)(RingBuffer(T) *)__attribute__((warn_unused_result)); \
-        bool (*has_data)(RingBuffer(T));                                      \
-        void (*clear)(RingBuffer(T) *);                                       \
-    };                                                                        \
-                                                                              \
-    static inline bool rbuf_push(T)(RingBuffer(T) * rbuf, T value) {          \
-        const size_t next = (rbuf->head + 1) % rbuf->size;                    \
-        if (next == rbuf->tail) {                                             \
-            return false;                                                     \
-        }                                                                     \
-                                                                              \
-        rbuf->data[rbuf->head] = value;                                       \
-                                                                              \
-        rbuf->head = next;                                                    \
-                                                                              \
-        return true;                                                          \
-    }                                                                         \
-                                                                              \
-    static inline Option(T) rbuf_pop(T)(RingBuffer(T) * rbuf) {               \
-        if (rbuf->head == rbuf->tail) {                                       \
-            return None(T);                                                   \
-        }                                                                     \
-                                                                              \
-        T val = rbuf->data[rbuf->tail];                                       \
-                                                                              \
-        rbuf->tail = (rbuf->tail + 1) % rbuf->size;                           \
-                                                                              \
-        return Some(T, val);                                                  \
-    }                                                                         \
-                                                                              \
-    static inline bool rbuf_has_data(T)(RingBuffer(T) rbuf) {                 \
-        return rbuf.head != rbuf.tail;                                        \
-    }                                                                         \
-                                                                              \
-    static inline void rbuf_clear(T)(RingBuffer(T) * rbuf) {                  \
-        rbuf->head = rbuf->tail = 0;                                          \
+#define RingBufferImpl(T)                                                           \
+    typedef struct RingBuffer(T) RingBuffer(T);                                     \
+                                                                                    \
+    struct RingBuffer(T) {                                                          \
+        T *const     __data;                                                        \
+        const size_t __size;                                                        \
+        size_t       __head;                                                        \
+        size_t       __tail;                                                        \
+        const bool (*push)(RingBuffer(T) *, T) __attribute__((warn_unused_result)); \
+        const Option(T) (*pop)(RingBuffer(T) *)__attribute__((warn_unused_result)); \
+        const bool (*has_data)(RingBuffer(T));                                      \
+        const void (*clear)(RingBuffer(T) *);                                       \
+    };                                                                              \
+                                                                                    \
+    static inline bool rbuf_push(T)(RingBuffer(T) * rbuf, T value) {                \
+        const size_t next = (rbuf->__head + 1) % rbuf->__size;                      \
+        if (next == rbuf->__tail) {                                                 \
+            return false;                                                           \
+        }                                                                           \
+                                                                                    \
+        rbuf->__data[rbuf->__head] = value;                                         \
+                                                                                    \
+        rbuf->__head = next;                                                        \
+                                                                                    \
+        return true;                                                                \
+    }                                                                               \
+                                                                                    \
+    static inline Option(T) rbuf_pop(T)(RingBuffer(T) * rbuf) {                     \
+        if (rbuf->__head == rbuf->__tail) {                                         \
+            return None(T);                                                         \
+        }                                                                           \
+                                                                                    \
+        T val = rbuf->__data[rbuf->__tail];                                         \
+                                                                                    \
+        rbuf->__tail = (rbuf->__tail + 1) % rbuf->__size;                           \
+                                                                                    \
+        return Some(T, val);                                                        \
+    }                                                                               \
+                                                                                    \
+    static inline bool rbuf_has_data(T)(RingBuffer(T) rbuf) {                       \
+        return rbuf.__head != rbuf.__tail;                                          \
+    }                                                                               \
+                                                                                    \
+    static inline void rbuf_clear(T)(RingBuffer(T) * rbuf) {                        \
+        rbuf->__head = rbuf->__tail = 0;                                            \
     }
 
 /**
@@ -242,10 +237,10 @@ _Noreturn static inline void raise_error(const char *msg) {
  */
 #define rbuf_from(T, buf)             \
     {                                 \
-        .data     = (buf),            \
-        .size     = ARRAY_SIZE(buf),  \
-        .head     = 0,                \
-        .tail     = 0,                \
+        .__data   = (buf),            \
+        .__size   = ARRAY_SIZE(buf),  \
+        .__head   = 0,                \
+        .__tail   = 0,                \
         .push     = rbuf_push(T),     \
         .pop      = rbuf_pop(T),      \
         .has_data = rbuf_has_data(T), \
