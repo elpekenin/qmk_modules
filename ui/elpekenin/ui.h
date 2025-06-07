0f9b7871e36bfa2525ddd65096888e55a0568dcb
elpekenin <elpekenin@elpekenin.dev>
2025-06-07 18:23:07 +0200

    first `ui` docs

--- ui/elpekenin/ui.h
+++ ui/elpekenin/ui.h
@@ -2,18 +2,20 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 /**
- * Composable UI design.
+ * Composable design of GUIs over QP, based on a hierarchy of nodes.
+ *
+ * This system was designed for flexibility, because computing each element's position and size by hand was a PITA and prone to errors.
+ * It also wasn't dynamic, in the sense that if some element of the screen wasn't being drawn (eg `#if`'ed away), sizes wouldn't adapt and leave a gap.
  */
 
 // -- barrier --
 
 #pragma once
 
-// #if !defined(QUANTUM_PAINTER_ENABLE)
-// #    error Quantum painter must be enabled to use ui
-// #endif
+#if !defined(QUANTUM_PAINTER_ENABLE)
+#    error Quantum painter must be enabled to use ui
+#endif
 
-#include <errno.h>
 #include <quantum/painter/qp.h>
 #include <quantum/util.h>
 #include <stdbool.h>
@@ -77,23 +79,46 @@ typedef struct _ui_node_t {
     void (*const render)(const struct _ui_node_t *, painter_device_t);
 } ui_node_t;
 
-//
+/**
+ * A node can have children, this is represented by:
+ *    - ``.children = UI_CHILDREN(nodes)``: Where ``nodes`` is an array of ``ui_node_t``'s
+ *
+ * To compute the size of each child, parents must specify how their size will be shared between all children.
+ *    - ``.split_direction = UI_SPLIT_DIR_HORIZONTAL``: All children are as tall as parent, and size splits horizontally
+ *    - ``.split_direction = UI_SPLIT_DIR_VERTICAl``: All children are as wide as parent, and size splits vertically
+ */
+#define UI_CHILDREN(x)                \
+    (ui_children_t) {                 \
+        .ptr = x, .n = ARRAY_SIZE(x), \
+    }
 
+/**
+ * Every node must define how big they want to be by setting ``.node_size`` to the following macros:
+ */
+
+/**
+ * ``x`` pixels in size
+ */
 #define UI_ABSOLUTE(x)                             \
     (ui_node_size_t) {                             \
         .mode = UI_SPLIT_MODE_ABSOLUTE, .size = x, \
     }
 
+/**
+ * ``x`` % of parent's size
+ */
 #define UI_RELATIVE(x)                             \
     (ui_node_size_t) {                             \
         .mode = UI_SPLIT_MODE_RELATIVE, .size = x, \
     }
 
 /**
- * Size equal to font's height. Can only be used within vertical split node.
+ * Size equal to a font's height. Can only be used within vertical split node.
  *
  * .. warning::
- *    Accessed node->args as a font to load.
+ *   Executes``qp_load_font_mem(*(void**)node->args)`` to compute size.
+ *   That is, the node's ``args`` **must** point to a structure whose first element
+ *   is a font's array.
  */
 #define UI_FONT()                   \
     (ui_node_size_t) {              \
@@ -101,31 +126,50 @@ typedef struct _ui_node_t {
     }
 
 /**
- * Size equal to image's width/height (depending on parent's split direction).
+ * Size equal to a image's width/height (depending on parent's split direction).
  *
  * .. warning::
- *    Accessed node->args as an image to load.
+ *   Executes``qp_load_image_mem(*(void**)node->args)`` to compute size.
+ *   That is, the node's ``args`` **must** point to a structure whose first element
+ *   is an image's array.
  */
 #define UI_IMAGE()                   \
     (ui_node_size_t) {               \
         .mode = UI_SPLIT_MODE_IMAGE, \
     }
 
+/**
+ * Claim the parent's remaining (not used by siblings) size.
+ */
 #define UI_REMAINING()                   \
     (ui_node_size_t) {                   \
         .mode = UI_SPLIT_MODE_REMAINING, \
     }
 
-//
-
-#define UI_CHILDREN(x)                \
-    (ui_children_t) {                 \
-        .ptr = x, .n = ARRAY_SIZE(x), \
-    }
-
-//
-
+/**
+ * Once you've declared a node tree, use this function to compute all nodes' size/position.
+ *
+ * If the input can't be resolved (eg children don't fit into parent), function will flag the node as invalid, and return false.
+ *
+ * .. hint::
+ *   If a node must run some validation (eg: its computed height >= font used), it can provide an ``.init`` function.
+ *   Returning ``false`` from it means that requirements weren't met, causing tree's resolution to fail.
+ */
 bool ui_init(ui_node_t *root, ui_coord_t width, ui_coord_t height);
+
+/**
+ * You shall use this function to render all nodes.
+ *
+ * Each node describes how it's rendered by providing a ``.render`` function.
+ * If it needs to track some state, it may use the ``.args`` field to store a pointer into whichever structure.
+ *
+ * Usually, you'd want run this function periodically (ie: from ``housekeeping_task_user``), but note that there isn't any kind of rate-limiting,
+ * if a node shall only be drawn every once in a while, this has to be handled on the node's logic.
+ *
+ * .. warning::
+ *   There isn't any kind of limitation to the area in which each node can draw.
+ *   This means that nodes are expected to respect the boundaries provided, with checks like ``if (qp_textwidth(font, text) > self->size.x) return;``
+ */
 bool ui_render(const ui_node_t *parent, painter_device_t display);
 
 // for debugging
