b12ecf9f4825c40131eadbeb98e3a0952a5cc02b
David Young <davidyoung@planetbids.com>
2025-09-27 18:46:31 -0700

    Create elpekenin/colors module

---
+++ colors/colors.c
@@ -0,0 +1,28 @@
+#include "elpekenin/colors.h"
+
+#include "quantum.h"
+
+int get_rgb(color_t color, rgb_t *rgb) {
+    switch (color.type) {
+        case COLOR_TYPE_RGB:
+            *rgb = color.rgb;
+            break;
+
+        case COLOR_TYPE_HSV:
+            *rgb = hsv_to_rgb(color.hsv);
+            break;
+
+        case COLOR_TYPE_HUE:
+            *rgb = hsv_to_rgb((hsv_t){
+                .h = color.hsv.h,
+                .s = rgb_matrix_get_sat(),
+                .v = rgb_matrix_get_val(),
+            });
+            break;
+
+        default:
+            return -EINVAL;
+    }
+
+    return 0;
+}
