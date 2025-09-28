447001b77beb86dca5e79c85c48978e8f57c0911
David Young <davidyoung@planetbids.com>
2025-09-27 18:46:51 -0700

    Move colors definitions out of indicators

--- indicators/indicators.c
+++ indicators/indicators.c
@@ -1,6 +1,7 @@
 // Copyright Pablo Martinez (@elpekenin) <elpekenin@elpekenin.dev>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include "elpekenin/colors.h"
 #include "elpekenin/indicators.h"
 
 #include "quantum.h"
@@ -59,31 +60,6 @@ static uint8_t mod_config_8bit(uint8_t mod) {
     }
 
     return mod;
-}
-
-static int get_rgb(color_t color, rgb_t *rgb) {
-    switch (color.type) {
-        case COLOR_TYPE_RGB:
-            *rgb = color.rgb;
-            break;
-
-        case COLOR_TYPE_HSV:
-            *rgb = hsv_to_rgb(color.hsv);
-            break;
-
-        case COLOR_TYPE_HUE:
-            *rgb = hsv_to_rgb((hsv_t){
-                .h = color.hsv.h,
-                .s = rgb_matrix_get_sat(),
-                .v = rgb_matrix_get_val(),
-            });
-            break;
-
-        default:
-            return -EINVAL;
-    }
-
-    return 0;
 }
 
 //
