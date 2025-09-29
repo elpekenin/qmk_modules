28de29439106a43d4039f33d363c3e5b0e7c2819
David Young <101432365+dmyoung9@users.noreply.github.com>
2025-09-29 10:00:33 -0700

    Update indicators/indicators.c
    Co-authored-by: Pablo Martínez <58857054+elpekenin@users.noreply.github.com>

--- indicators/indicators.c
+++ indicators/indicators.c
@@ -1,7 +1,6 @@
 // Copyright Pablo Martinez (@elpekenin) <elpekenin@elpekenin.dev>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "elpekenin/colors.h"
 #include "elpekenin/indicators.h"
 
 #include "quantum.h"
